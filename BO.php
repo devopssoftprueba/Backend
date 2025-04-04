e9344af7fd0f59a64731a22f542cdf0f7ea14e16
Ronald Pelaez <ronald.jimenez@virtualsoft.tech>
2025-04-04 10:50:18 -0500

    prueba documentación buena

--- BO.php
+++ BO.php
@@ -1,14 +1,11 @@
 <?php
 
-// Falta la documentación PHPDoc en la clase y en el método
+namespace App;
 
-class BO {
-    public function authenticate(string $username, string $password): bool {
-        // Lógica de autenticación...
-        return true;
+class BO
+{
+    public function sumar($a, $b)
+    {
+        return $a + $b;
     }
 }
-
-
-
-
