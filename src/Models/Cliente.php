dabbfd2ec191a2d481163174c5b6850e3004b979
Ronald Pelaez <ronald.jimenez@virtualsoft.tech>
2025-04-11 15:45:51 -0500

    se agrega un nuevo auxiliar para el script

---
+++ src/Models/Cliente.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace models;
+
+/**
+ * Class Cliente
+ *
+ * Clase que representa un cliente en el sistema.
+ *
+ * @category Models
+ * @package  App\Models
+ * @author   Ronald
+ * @version  1.0
+ * @since    Archivo disponible desde la versión 1.0
+ */
+
+class Cliente //esta clase esta siendo agregada de manera local, no debe fallar la automatizacion
+{
+    /**
+     * Identificador único del cliente.
+     *
+     * @var integer
+     */
+    private $id;
+
+    /**
+     * Nombre completo del cliente.
+     *
+     * @var string $nombre Nombre del cliente.
+     */
+    private $nombre;
+
+    /**
+     * Obtiene el nombre del cliente.
+     *
+     * @return string Nombre del cliente.
+     */
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+}
