b2881f35220e6ae44117bec2144e2ad26645b48a
TatianaJaimes05 <monica.jaimes@virtualsoft.tech>
2025-04-14 14:40:37 -0500

    se elimina comentario

--- src/controllers/ProductController.php
+++ src/controllers/ProductController.php
@@ -43,7 +43,6 @@ class ProductController
                     (float) $row['price']
                 );
             }
-            // Si no se encuentran productos, se devuelve un arreglo vacío
             return $products;
         } catch (PDOException $e) {
             return [];
